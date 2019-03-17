876ae3cfce1a68b431e8e2ff622818d22465ba30
cuijinyu <c2510271615@gmail.com>
2019-03-17 16:48:11 +0800

    Update:更正了志愿者页面的文字展示格式

--- src/pages/volunteer.js
+++ src/pages/volunteer.js
@@ -14,18 +14,163 @@ class Comming extends Component {
                 <div className="sosconf-volunteer__card-header">
                     <span>技术组</span>
                 </div>
-            </div>
-            <div className="sosconf-volunteer__card">
-                <div className="sosconf-volunteer__card-header">
-                    <span>技术组</span>
+                <div className="sosconf-volunteer__card-text">
+                    <p>按照当前的进度考虑和技术复杂度, 以及考虑在开发中培养更多同学的技术能力, 技术组目前计划人员需求如下:</p>
+                    <ul>
+                        <li>前端组*3 [ ]</li>
+                        <li>后端组*3 [ ]</li>
+                        <li>前端设计小组(包含UI) *2 [ ]</li>
+                        <li>运维(包含各种注册/查询系统维护)*2 [ ]</li>
+                        <li>萌新若干 [ ]</li>
+                    </ul>
+
+                    <p>注: 其中DevOps部分由后端组配合运维组共同维护</p>
+                    <p>以上为考虑中较为合理的配置, 并且包含想要通过技术组工作锻炼的萌新</p>
+                    <p>上述人数需求并非硬性限制, 如果有更多同学也欢迎加入我们技术组的开发工作(欢迎萌新)</p>
                 </div>
             </div>
+
             <div className="sosconf-volunteer__card">
                 <div className="sosconf-volunteer__card-header">
-                    <span>技术组</span>
+                    <span>设计组</span>
                 </div>
+                <div className="sosconf-volunteer__card-text">
+                
+
+                    <p>设计组是一个（可能）偏离技术路线，但对于活动的举办有锦上添花的作用的画风清奇小组。</p>
+                    <p>人员要求：</p>
+                    
+
+
+
+                    <ul>
+                        <li>不鸽（分配的任务可以及时完成）</li>
+                        <li>善于交流思路，说出想法</li>
+                        <li>不介意真的是萌新还是假装自己是萌新的同学</li>
+                        <li>无人数要求</li>
+                    </ul>
+
+                    <p>具体的工作有：</p>
+                    
+
+
+
+
+                    <ul>
+                        <li>视频剪辑</li>
+                        <li>宣传资料设计，如海报、宣传单、展板……</li>
+                        <li>网页设计和手机应用界面设计</li>
+                        <li>纪念品设计</li>
+                        <li>会场主视觉设计</li>
+                        <li>其他有新奇创意的设计（emm）</li>
+                    </ul>
+
+                    <p>
+                    技能要求（以下要求全是可选项）：
+                    </p>
+
+                    <ul>
+                        <li>会使用PS、AI或ID来制作宣传资料（当然，熟练使用GIMP那类的开源软件也可以）</li>
+                        <li>会使用Pr、AE等软件来剪辑视频</li>
+                        <li>Keynote或者PowerPoint</li>
+                        <li>电脑绘画</li>
+                        <li>会使用科学上网</li>
+                    </ul>
+                    <p>如果以上技能都没有……也没有关系，只要有兴趣和毅力就可以加入，我们可以带你🤣</p>
+                    <p>以上配置不一定完全合理且周到，欢迎有兴趣的同学联系。</p>
+                </div>
+            </div>
+
+            <div className="sosconf-volunteer__card">
+                <div className="sosconf-volunteer__card-header">
+                    <span>议程组</span>
+                </div>
+                <div className="sosconf-volunteer__card-text">
+                    <p>和我们一起创作第一次学生开源年会，见证历史。</p>
+                    <p>需求</p>
+                    <ul>
+                        <li>喜欢开源开发和文化或对开源感兴趣。</li>
+                        <li>喜欢团队合作。</li>
+                        <li>有相关经验者优先。</li>
+                        <li>认同我们的目标并有意长期和我们合作者优先！</li>
+                    </ul>
+
+                    <p>工作</p>
+                    <ul>
+                        <li>在前期准备中，我们将一起根据赞助商和讲者招募情况创建多个版本的议程时间表。</li>
+                        <li>我们会评估各类可能影响会议顺利开展的不确定性和风险，并准备相应的应对计划。</li>
+                        <li>我们会一起策划一些将随会议进行同时开展的具体项目（如学生开源项目展览），并和其他相关小组研究可行性。</li>
+                    </ul>
+                    <p>收获</p>
+                    <ul>
+                        <li>本次会议将是一次最独特的学习经历，你也能免费参与会议，并得到志愿者专属的赠品。</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div className="sosconf-volunteer__card">
+                <div className="sosconf-volunteer__card-header">
+                    <span>赞助组</span>
+                </div>
+                <div className="sosconf-volunteer__card-text">
+                    <p>赞助组主要负责对外联系潜在的赞助商，商务谈判和招募启动资金，也会行使一部分联系媒体或社区的职能。（是 sosconf 联系社会的纽带）</p>
+                    <p>人员要求</p>
+                    <ul>
+                        <li>不限国籍</li>
+                        <li>学生身份（不限于中学生、大学生、硕博生、留学生</li>
+                        <li>请优先分配女生，目前组内男生过于饱和（并不是说不要男生啦~而是在人手盈余和条件满足的情况下，请优先分配女生( •̀ ω •́ )y</li>
+                        <li>善于交流思路，口头表达能力较好</li>
+                        <li>具备一定的社交礼仪（现实 or 网络</li>
+                        <li>无人数要求</li>
+                    </ul>
+                    <p>技能要求（除了外语水平是硬性需求，其他均是可选项）</p>
+                    <ul>
+                        <li>英语六级 & 雅思5分及以上 & 托福60分及以上（满足任一条件即可</li>
+                        <li>口语流利和不怯场，能够同外国人进行一般及以上程度的的交流</li>
+                        <li>外语&法律/金融/计算机相关背景的同学（优先级从左到右依次排列，前两者是并列关系，起草赞助合同需要法学背景的同学帮忙</li>
+                        <li>会科学上网（约定使用统一的沟通模式，邮件+Skype</li>
+                        <li>撰写文案能力较强，思维活跃</li>
+                    </ul>
+                    <p>具体的工作</p>
+                    <ul>
+                        <li>制定年度 Call for Sponsors</li>
+                        <li>牵头起草赞助合同</li>
+                        <li>联系赞助商，与赞助商进行谈判，尽力促成赞助（每个人负责哪几家公司，跟进哪几条线索</li>
+                        <li>作为赞助商、媒体、社区三者之间的纽带，协助其他组共同推进事务进度</li>
+                        <li>运营和维护赞助商及合作伙伴的良好关系</li>
+                    </ul>
+                    <p>以上配置不一定完全合理且周到，欢迎有兴趣的同学加入并提出建议！</p>
+                </div>
+                
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
             </div>
         </div>
+
         <div className="sosconf-volunteer__button-group">
             <a href="./" className="animated bounce">APPLY</a>
         </div>
