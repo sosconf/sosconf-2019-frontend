3ea5af067917a8cfa3edff41a9897858bc34ec58
cuijinyu <c2510271615@gmail.com>
2019-03-18 13:33:24 +0800

    Update:更新了构建脚本

--- build.sh
+++ build.sh
@@ -1,1 +1,8 @@
+touch ~/.bashrc # Linux 不需要
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
+nvm install v10.1.0
+nvm use v10.1.0
+sudo npm install yarn cnpm -g 
+sudo cnpm install react-scripts -g #测试之后，使用npm安装递归非常大的依赖解决方案是使用cnpm
+yarn install
 npm run build
