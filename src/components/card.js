1a07a8610c8c8426fde4cfd63e76f8a3b6329041
cuijinyu <c2510271615@gmail.com>
2019-03-19 14:42:34 +0800

    Update:修复了字体问题,设置英文字体为Comfortaa,中文字体为雅黑

---
+++ src/components/card.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import '../themes/card.css';
+
+class Card extends Component {
+    constructor() {
+
+    }
+    render() {
+        <div className="sosconf__card">
+            <div className="sosconf__card-wrap">
+                <div>
+                    
+                </div>
+            </div>
+        </div>
+    }
+}
