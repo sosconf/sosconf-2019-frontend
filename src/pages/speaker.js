9463403292fe0fd44931ff2326a81b3c023c0b27
cuijinyu <c2510271615@gmail.com>
2019-04-25 21:36:08 +0800

    docs:修改了一些文案

--- src/pages/speaker.js
+++ src/pages/speaker.js
@@ -7,6 +7,9 @@ class Speaker extends Component {
     return (
       <div className="sosconf-comming__wrap">
         <p className="sosconf-coming__text">
+        <div>
+          <button className="sosconf-speaker__button">填写申请</button>
+        </div>
         <FormattedMessage id="pleaseContact"/> speakers@sosconf.org</p>
       </div>
     );
