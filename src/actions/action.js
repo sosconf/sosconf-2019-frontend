86879a37e3a487d1cbd5760675814fe2f0fbf75f
cuijinyu <c2510271615@gmail.com>
2019-05-02 22:08:10 +0800

    fix:暂时将注册登录屏蔽

--- src/actions/action.js
+++ src/actions/action.js
@@ -1,6 +1,7 @@
 export const LANGUAGE_ENGLISH = "LANGUAGE_ENGLISH";
 export const LANGUAGE_CHINESE = "LANGUAGE_CHINESE";
 export const CHANGE_TICKET = "CHANGE_TICKET";
+export const CHANGE_USER = "CHANGE_USER";
 
 export const changeToEnglish = () => ({
     type: LANGUAGE_ENGLISH
@@ -12,4 +13,9 @@ export const changeToChinese = () => ({
 
 export const changeTicket = () => ({
     type: CHANGE_TICKET
+})
+
+export const changeUser = userProfile => ({
+    type: CHANGE_USER,
+    userProfile
 })
